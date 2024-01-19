e906b84a28ca36188b6511fced7bbb40cb7f53b0
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-01-19 08:54:05 -0600

    Refactor code to use Stack instead of List

--- Program.cs
+++ Program.cs
@@ -4,90 +4,27 @@ namespace Colecciones
     {
         static void Main(string[] args)
         {
-            int opc, indice;
-            string alumno;
+            // Stack
+            Stack<double> MiPila = new Stack<double>();
+            // Insertar objetos al principio del Stack
+            MiPila.Push(5.9);
+            MiPila.Push(8.2);
+            MiPila.Push(54.2);
+            MiPila.Push(91.1);
 
-            // Instancia de List
-            List<string> Alumnos = new List<string>();
-            do
+            int i = 0;
+            foreach (double item in MiPila)
             {
-                Console.Clear();
-                Console.WriteLine("1. Agregar estudiante");
-                Console.WriteLine("2. Eliminar estudiante");
-                Console.WriteLine("3. Mostrar estudiantes");
-                Console.WriteLine("4. Buscar por nombre");
+                Console.WriteLine("{0}. {1}", i++, item);
+            }
 
-                // Asignar opcion
-                opc = Convert.ToInt32(Console.ReadLine());
-
-                Console.Clear();
-
-                switch (opc)
-                {
-                    case 1:
-                        Console.Write("Digita el nombre del estudiante que desees agregar a la lista: ");
-                        alumno = Console.ReadLine() ?? "";
-                        Alumnos.Add(alumno);
-                        break;
-
-                    case 2:
-                        // Console.Write("Escribe el nombre del alumno que desees eliminar: ");
-                        // alumno = Console.ReadLine() ?? "";
-                        // Alumnos.Remove(alumno);
-
-                        Console.Write("Escribe el numero del estudiante que quiere eliminar ");
-                        indice = Convert.ToInt32(Console.ReadLine());
-                        indice--; // Reducimos en 1 al indice
-
-
-                        if (indice >= Alumnos.Count || indice < 0)
-                        {
-                            Console.WriteLine("El alumno no existe");
-                        }
-                        else
-                        {
-                            string alumnoEliminado = Alumnos[indice];
-                            Alumnos.RemoveAt(indice);
-                            Console.WriteLine("{0} se ha eliminado satisfactoriamente", alumnoEliminado);
-                        }
-                        ConsolePause();
-
-                        break;
-
-                    case 3:
-                        int i = 1;
-                        foreach (string estudiante in Alumnos)
-                        {
-                            Console.WriteLine("{0}. {1}", i++, estudiante);
-                        }
-                        ConsolePause();
-                        break;
-                    case 4:
-                        // string encontrarAlumno;
-                        int j;
-                        Console.WriteLine("Ingrese el nombre del estudiante a buscar: ");
-                        alumno = Console.ReadLine() ?? "";
-
-                        j = Alumnos.IndexOf(alumno);
-                        if (j != -1)
-                        {
-                            Console.WriteLine("El alumno {0} tiene el numero de lista {1}",
-                             Alumnos[j], j + 1);
-                        }
-                        else
-                        {
-                            Console.WriteLine("No se ha encontrado el alumno: {0}", alumno);
-                        }
-                        ConsolePause();
-                        break;
-                }
-            } while (opc >= 1 && opc <= 4);
-        }
-
-        static void ConsolePause()
-        {
-            Console.WriteLine("Presiona cualquier tecla para continuar...");
-            Console.ReadLine();
+            i = 0;
+            Console.WriteLine("\nDespues de insertar un elemento");
+            MiPila.Push(77.7);
+            foreach (double item in MiPila)
+            {
+                Console.WriteLine("{0}. {1}", i++, item);
+            }
         }
     }
 }
